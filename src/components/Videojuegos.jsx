757ce9596ef7dffec3dddee5d5bd13a33d5cc980
danielVillegasCelaya <dvillegasc02@educantabria.es>
2022-01-19 13:02:48 +0100

    tabla videjuegos insertada, falta que se pueda hacer click

---
+++ src/components/Videojuegos.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Card, Container, Table, Row, Col } from 'react-bootstrap';
+import {
+  TitulosTablaVideojuegos,
+  DatosTablaVideojuegos,
+} from '../data/DatosVideojuegos';
+class Videojuegos extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="main-site">
+        <h1>Videojuegos</h1>
+        <Container>
+          <Row>
+            <Col lg={8} md={6}>
+              <Table responsive striped>
+                <thead>
+                  <tr>
+                    <th>{TitulosTablaVideojuegos.id}</th>
+                    <th>{TitulosTablaVideojuegos.categoria}</th>
+                    <th>{TitulosTablaVideojuegos.multijugador}</th>
+                    <th>{TitulosTablaVideojuegos.historia}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {DatosTablaVideojuegos.map((item) => {
+                    return (
+                      <tr>
+                        <td>{item.Nombre}</td>
+                        <td>{item.Categoria}</td>
+                        <td>{item.Multijugador}</td>
+                        <td>{item.Historia}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Col>
+            <Col lg={4} md={6}>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={DatosTablaVideojuegos[2].Imagen} />
+                <Card.Body>
+                  <Card.Title>
+                    {DatosTablaVideojuegos[2].Categoria}{' '}
+                    {DatosTablaVideojuegos[2].Multijugador}
+                  </Card.Title>
+                  <Card.Text>
+                    Categoria: {DatosTablaVideojuegos[2].Categoria}
+                    <p />
+                    Multijugador: {DatosTablaVideojuegos[2].Multijugador}
+                    <p />
+                    Historia: {DatosTablaVideojuegos[2].Historia}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Videojuegos;
---
+++ src/data/DatosVideojuegos.js
@@ -0,0 +1,34 @@
+export const TitulosTablaVideojuegos = {
+  id: 'Nombre',
+  categoria: 'Categoria',
+  multijugador: 'Multijugador',
+  historia: 'Historia',
+  imagen: 'Imagen',
+};
+
+// TODO: rellenar atributos que faltan para todos los videojuegos
+export const DatosTablaVideojuegos = [
+  {
+    Nombre: 'Dark Souls Remastered',
+    Categoria: 'Souls-like',
+    Multijugador: 'Coop/PvP',
+    Historia: 'Si',
+    Imagen: 'https://i.blogs.es/591b5a/280518-darksouls-review/1366_2000.jpg',
+  },
+  {
+    Nombre: 'FARCRY 6',
+    Categoria: 'Mundo abierto',
+    Multijugador: 'Coop',
+    Historia: 'Si',
+    Imagen:
+      'https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/Bn213V7aySLmjGwfQkSMy/bffebc6e9a19f3524a306d89cbc3b0d4/fc6-page_meta-thumbnail.jpg',
+  },
+  {
+    Nombre: 'Escape from Tarkov',
+    Categoria: 'FPS/Hardcore',
+    Multijugador: 'Si',
+    Historia: 'Misiones y lore',
+    Imagen:
+      'https://www.esportmaniacos.com/wp-content/uploads/2021/12/escape-from-tarkov-nvidia-reflex-available-now-ogimage1.jpg',
+  },
+];
