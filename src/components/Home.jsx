78c43a249fc6325e516e32c52482d40cf8b4e790
danielVillegasCelaya <dvillegasc02@educantabria.es>
2022-01-25 11:47:29 +0100

    Bienvenido usuario con el registro del nombre

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,33 +4,67 @@ import { Form, Container, Button } from 'react-bootstrap';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: '',
+      contrasena: '',
+    };
+    this.login = this.login.bind(this);
+    this.userR = React.createRef();
+    this.contrasenaR = React.createRef();
+  }
+
+  login() {
+    this.setState({
+      user: this.userR.current.value,
+      contrasena: this.contrasenaR.current.value,
+    });
   }
 
   render() {
-    return (
-      <div className="login">
-        <h1>Login</h1>
-        <Container>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+    if (
+      this.state !== null &&
+      this.state.user !== null &&
+      this.state.user !== ''
+    ) {
+      return (
+        <div className="main-site">
+          <h1>Bienvenido {this.state.user}!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="login">
+          <h1>Login</h1>
+          <Container>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Usuario"
+                  ref={this.userR}
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Container>
-      </div>
-    );
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  ref={this.contrasenaR}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Recordarme" />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={this.login}>
+                Login
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      );
+    }
   }
 }
 export default Home;
