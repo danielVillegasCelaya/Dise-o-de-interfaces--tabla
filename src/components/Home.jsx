db455ea0572b62af3b91d242f36a72ca36c89564
danielVillegasCelaya <dvillegasc02@educantabria.es>
2022-01-25 10:27:04 +0100

    Login insertado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Button from 'react-bootstrap/esm/Button';
+import { Form } from 'react-bootstrap/esm/';
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-export function Home() {
-  return (
-    <div className="main-site">
-      <h1>Bienvenido!</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div className="login">
+        <h1>Login</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
+export default Home;
