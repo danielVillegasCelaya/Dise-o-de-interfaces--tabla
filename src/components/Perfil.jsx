d7506b38836a601eefba01a5593197f340c68424
danielVillegasCelaya <dvillegasc02@educantabria.es>
2022-01-26 21:47:02 +0100

    algo avanzado pero sin resultados

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,22 +1,68 @@
 import React from 'react';
-import Home from './Home';
+import {CamposUsuario, DatosUsuarios} from '../data/DatosUsuarios'
 
-export function Perfil() {
+class Perfil extends React.Component {
   constructor(props) {
-    super(props);
     this.state = {
       user: '',
       contrasena: '',
     };
+    this.logout=this.logout.bind(this);
+
   }
-  };
+
+  logout() {
+    this.setState({
+      user: '',
+      contrasena: '',
+    });
+  }
+
+  recogerDetallesUsuario(e) {
+    for (let i = 0; i < DatosUsuarios.length; i++) {
+      if (e == DatosUsuarios[i].Nombre) {
+        this.setState({ usuario: i });
+        break;
+      }
+    }
+  }
 
 
-  return (
-    <div className="main-site">
-      
-      <h1>Página de perfil</h1>
-      <h3>Nombre</h3>
-    </div>
-  );
+  componentDidMount() {
+    this.setState({
+      user: localStorage.getItem('user'),
+      contrasena: localStorage.getItem('contrasena'),
+    });
+  }
+
+  render(){
+    return(
+      <div><tbody>
+      {DatosUsuarios.map((item) => {
+        return (
+          <h1
+            key={uuid()}
+            onClick={() =>
+              this.recogerDetallesUsuario(user)
+            }
+          >
+            <h2>Nombre {item.Nombre}</h2>
+            <h2>Contraseña {item.Contrasena}</h2>
+            <h2>Ciudad {item.Ciudad}</h2>
+            <h2>Email {item.Email}</h2>
+            <h2>Foto {item.Imagen}</h2>
+          </h1>
+        );
+      })}
+    </tbody>
+        <h2>Nombre</h2>
+        <h2>Contraseña</h2>
+        <h2>Ciudad</h2>
+        <h2>Email</h2>
+        <h2>Foto</h2>
+      </div>  
+    );
+  }
+
+
 }
--- src/data/DatosUsuarios.js
+++ src/data/DatosUsuarios.js
@@ -16,4 +16,12 @@ export const DatosUsuarios = [
     Imagen:
       'https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/01/14/16106414293626.jpg',
   },
+  {
+    Nombre: 'Victor',
+    Contrasena: 'Victor',
+    Ciudad: 'Santander',
+    Email: 'victor@victor',
+    Imagen:
+      'https://pbs.twimg.com/profile_images/1420330650545106954/3Cd4EKB3_400x400.jpg',
+  },
 ];
